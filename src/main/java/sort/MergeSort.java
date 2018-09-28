e4c02d52284acd93842c4114d700d313e9be6550
wangchenjun <dabai4@163.com>
2018-09-28 15:40:16 +0800

    MergeSort implement complete

---
+++ src/main/java/sort/MergeSort.java
@@ -0,0 +1,56 @@
+package sort;
+
+import java.util.Random;
+
+public class MergeSort {
+    public static void mergeSort(int[] a, int beg, int end) {
+        if (beg >= end)
+            return;
+        int mid = (end - beg) / 2 + beg;
+        mergeSort(a, beg, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, beg, mid, end);
+    }
+
+    private static void merge(int[] a, int beg, int mid, int end) {
+        int[] temp = new int[end - beg + 1];
+        int ll = beg, hl = mid + 1;
+        for (int i = 0; i < temp.length; i++) {
+            if (ll > mid) {
+                temp[i] = a[hl];
+                hl++;
+            }else if (hl > end) {
+                temp[i] = a[ll];
+                ll++;
+            } else {
+                if (a[ll] > a[hl]) {
+                    temp[i] = a[hl];
+                    hl++;
+                } else {
+                    temp[i] = a[ll];
+                    ll++;
+                }
+            }
+        }
+        for (int i = 0; i < temp.length; i++) {
+            a[beg] = temp[i];
+            beg++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        long beg = System.currentTimeMillis();
+        Random random = new Random();
+        int a[] = new int[10000000];
+        for (int i = 0; i < a.length; i++)
+            a[i] = random.nextInt();
+        mergeSort(a, 0, a.length - 1);
+        for (int i = 0; i < a.length - 1; i++)
+            if (a[i] > a[i + 1])
+                System.out.println("wrong"); //检查排序是否出错
+//        for (int i = 0; i < a.length; i++)
+//            System.out.print(a[i] + " "); //打印出序列
+        System.out.println("\n" + (System.currentTimeMillis() - beg)); //耗时
+    }
+}
