af359fa8443e59997697508f258633b9e7bbda26
wangchenjun <dabai4@163.com>
2018-10-11 10:56:04 +0800

    MergeSort new version

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -12,30 +12,45 @@ public class MergeSort {
         merge(a, beg, mid, end);
     }
 
+//    private static void merge(int[] a, int beg, int mid, int end) {
+//        int[] temp = new int[end - beg + 1];
+//        int ll = beg, hl = mid + 1;
+//        for (int i = 0; i < temp.length; i++) {
+//            if (ll > mid) {
+//                temp[i] = a[hl];
+//                hl++;
+//            }else if (hl > end) {
+//                temp[i] = a[ll];
+//                ll++;
+//            } else {
+//                if (a[ll] > a[hl]) {
+//                    temp[i] = a[hl];
+//                    hl++;
+//                } else {
+//                    temp[i] = a[ll];
+//                    ll++;
+//                }
+//            }
+//        }
+//        for (int i = 0; i < temp.length; i++) {
+//            a[beg] = temp[i];
+//            beg++;
+//        }
+//
+//    }
     private static void merge(int[] a, int beg, int mid, int end) {
         int[] temp = new int[end - beg + 1];
-        int ll = beg, hl = mid + 1;
-        for (int i = 0; i < temp.length; i++) {
-            if (ll > mid) {
-                temp[i] = a[hl];
-                hl++;
-            }else if (hl > end) {
-                temp[i] = a[ll];
-                ll++;
-            } else {
-                if (a[ll] > a[hl]) {
-                    temp[i] = a[hl];
-                    hl++;
-                } else {
-                    temp[i] = a[ll];
-                    ll++;
-                }
-            }
-        }
-        for (int i = 0; i < temp.length; i++) {
-            a[beg] = temp[i];
-            beg++;
-        }
+        int ll = beg, rl = mid + 1, index = 0;
+        while (ll <= mid && rl <= end)
+            temp[index++] = a[ll] < a[rl] ? a[ll++] : a[rl++];
+        for (; ll <= mid;)
+            temp[index++] = a[ll++];
+        for (; rl <= end;)
+            temp[index++] = a[rl++];
+        for (int i = 0; i < temp.length; i++)
+            a[beg++] = temp[i];
+
+
 
     }
 
@@ -44,7 +59,7 @@ public class MergeSort {
         Random random = new Random();
         int a[] = new int[10000000];
         for (int i = 0; i < a.length; i++)
-            a[i] = random.nextInt();
+            a[i] = random.nextInt() % 10;
         mergeSort(a, 0, a.length - 1);
         for (int i = 0; i < a.length - 1; i++)
             if (a[i] > a[i + 1])
